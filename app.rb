2c51f4d271b715396cd0dc28ac8931851520be60
Manton Reece <manton@me.com>
2022-07-17 13:37:42 -0500

    Started JSON API with simple GET.

--- app.rb
+++ app.rb
@@ -17,3 +17,30 @@ get "/" do
 	
 	erb :index
 end
+
+get "/places/nearby" do
+	content_type :json
+
+	lat = params[:latitude].to_f
+	long = params[:longitude].to_f
+	meters = params[:meters] || 10000
+	count = params[:count] || 50
+	
+	sql = "SELECT id, osm_id, name, type, icon, latitude, longitude, ST_Distance_Sphere(pt, ST_GeomFromText('POINT(#{long} #{lat})')) AS meters FROM places HAVING meters < #{meters.to_i} ORDER BY meters LIMIT #{count.to_i}"
+	places = db.fetch(sql)
+	
+	results = []
+	for place in places
+		results << {
+			id: place[:id],
+			osm_id: place[:osm_id],
+			name: place[:name],
+			type: place[:type],
+			icon: place[:icon],
+			latitude: place[:latitude],
+			longitude: place[:longitude]
+		}
+	end
+	
+	return JSON.pretty_generate(results)
+end
