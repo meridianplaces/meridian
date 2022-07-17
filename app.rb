7aa39fffc9d595274e0c393a656f09d9d058ac6c
Manton Reece <manton@me.com>
2022-07-17 13:25:30 -0500

    Updated to return 50 places.

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ get "/" do
 		pieces = @q.split(",")
 		lat = pieces.first.strip.to_f
 		long = pieces.last.strip.to_f
-		sql = "SELECT id, name, type, icon, ST_AsText(pt), ST_Distance_Sphere(pt, ST_GeomFromText('POINT(#{long} #{lat})')) AS meters FROM places HAVING meters < 100000 ORDER BY meters LIMIT 20"
+		sql = "SELECT id, name, type, icon, ST_AsText(pt), ST_Distance_Sphere(pt, ST_GeomFromText('POINT(#{long} #{lat})')) AS meters FROM places HAVING meters < 100000 ORDER BY meters LIMIT 50"
 		@places = db.fetch(sql)
 	else
 		@places = []
