d26b27a2c7d4e1b9a58badc6141092a76f3e8527
Manton Reece <manton@me.com>
2023-03-28 15:28:27 -0500

    Added app source and name fields for potential app-specific data and filtering. Endpoings for getting types/icons.

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ Bundler.require
 
 db = Sequel.connect(ENV["DATABASE_URL"])
 
+ICONS = JSON.parse(IO.read("config/icons.json"))
+TYPES = JSON.parse(IO.read("config/types.json"))
+
 get "/" do
 	@q = params[:q].to_s
 	
@@ -18,6 +21,36 @@ get "/" do
 	erb :index
 end
 
+get "/places/types" do
+	content_type :json
+	
+	flat_list = []
+	
+	TYPES.each do |key, val|
+		val.each do |sub_key, sub_val|
+			flat_list << sub_val
+		end
+	end
+	
+	flat_list.uniq!
+	return flat_list.to_json
+end
+
+get "/places/icons" do
+	content_type :json
+
+	flat_list = []
+	
+	ICONS.each do |key, val|
+		val.each do |sub_key, sub_val|
+			flat_list << sub_val
+		end
+	end
+	
+	flat_list.uniq!
+	return flat_list.to_json
+end
+
 get "/places/nearby" do
 	content_type :json
 
@@ -25,9 +58,15 @@ get "/places/nearby" do
 	long = params[:longitude].to_f
 	meters = params[:meters] || 10000
 	count = params[:count] || 50
+	app_source = params[:app_source]
 	
-	sql = "SELECT id, osm_id, name, type, icon_carto, icon_fontawesome, latitude, longitude, ST_Distance_Sphere(pt, ST_GeomFromText('POINT(#{long} #{lat})')) AS meters FROM places HAVING meters < #{meters.to_i} ORDER BY meters LIMIT #{count.to_i}"
-	places = db.fetch(sql)
+	if app_source.nil?
+		sql = "SELECT id, osm_id, name, type, icon_carto, icon_fontawesome, latitude, longitude, ST_Distance_Sphere(pt, ST_GeomFromText('POINT(#{long} #{lat})')) AS meters FROM places HAVING meters < #{meters.to_i} ORDER BY meters LIMIT #{count.to_i}"
+		places = db.fetch(sql)
+	else
+		sql = "SELECT id, osm_id, name, type, icon_carto, icon_fontawesome, latitude, longitude, ST_Distance_Sphere(pt, ST_GeomFromText('POINT(#{long} #{lat})')) AS meters FROM places WHERE app_source = ? HAVING meters < #{meters.to_i} ORDER BY meters LIMIT #{count.to_i}"
+		places = db.fetch(sql, app_source)
+	end
 	
 	results = []
 	for place in places
@@ -53,9 +92,11 @@ post "/places" do
 	type = params[:type].to_s
 	lat = params[:latitude].to_f
 	long = params[:longitude].to_f
+	app_source = params[:app_source].to_s
+	app_name = params[:app_name].to_s
 	
 	if name.length > 0	
-		insert_ds = db["INSERT INTO places (osm_id, osm_type, name, type, latitude, longitude, pt) VALUES (?, ?, ?, ?, ?, ?, ST_GeomFromText('POINT(#{long} #{lat})'))", 0, "", name, type, lat, long]
+		insert_ds = db["INSERT INTO places (osm_id, osm_type, name, type, latitude, longitude, pt, app_source, app_name) VALUES (?, ?, ?, ?, ?, ?, ST_GeomFromText('POINT(#{long} #{lat})'), ?, ?)", 0, "", name, type, lat, long, app_source, app_name]
 		insert_ds.insert
 	end
 	
